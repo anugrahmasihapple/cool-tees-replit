97d63ccdcf708dd81c1786feb726dc209cd5e10e
anugrahmasihapple <99712115+anugrahmasihapple@users.noreply.github.com>
2022-09-30 02:32:14 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,7 +31,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://frontend-electric-views.herokuapp.com']
+X_FRAME_OPTIONS = '*'
+CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/','http://0.0.0:3000']
 
 # Application definition
 
